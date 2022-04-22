5f5e0f713ee9fa962f5d60d826fb83f0c4ec012a
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2022-04-22 10:23:11 +0800

    feat(*): add center and subject

---
+++ DSTR-Assignment/center.h
@@ -0,0 +1,13 @@
+#ifndef CENTER_H_INCLUDED
+#define CENTER_H_INCLUDED
+
+#include <string>
+
+struct Center {
+    std::string ID;
+    std::string name;
+    Center();
+    ~Center();
+};
+
+#endif
---
+++ DSTR-Assignment/subject.h
@@ -0,0 +1,13 @@
+#ifndef SUBJECT_H_INCLUDED
+#define SUBJECT_H_INCLUDED
+
+#include <string>
+
+struct Subject {
+    std::string ID;
+    std::string name;
+    Subject();
+    ~Subject();
+};
+
+#endif

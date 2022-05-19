7b8072e6c44b6c048f01274e2d30492bee2b19e4
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2022-05-19 21:21:27 +0800

    feat(search): binary search

--- DSTR-Assignment/src/search.cpp
+++ DSTR-Assignment/src/search.cpp
@@ -3,23 +3,31 @@
 
 #include "display.h"
 #include "search.h"
+#include "binary_tree.h"
 
 using namespace std;
 
 TutorList searchTutor(TutorList &ll, int (*CompareFn)(Tutor *, Tutor *),
                       Tutor *t) {
-  TutorNode *temp = ll.head;
+  BinaryTree bt = BinaryTree(ll, (*CompareFn), 'a');
+  
+  TutorNode *treeNodePtr = bt.root;
   TutorList tempList;
-
-  while (temp != nullptr) {
-    int result = (*CompareFn)(temp->tutor, t);
-    if (result == 0) {
-      tempList.AddToLast(temp->tutor);
-    }
-
-    temp = temp->next;
-  }
   tempList.copy = true;
+
+  while (treeNodePtr != nullptr) {
+    int result = (*CompareFn)(t, treeNodePtr->tutor);
+    if (result == 0) {
+      tempList.AddToLast(treeNodePtr->tutor);
+      treeNodePtr = treeNodePtr->prev;
+    }
+    else if (result > 0) {
+      treeNodePtr = treeNodePtr->next;
+    }
+    else {
+      treeNodePtr = treeNodePtr->prev;
+    }
+  }
 
   return tempList;
 }

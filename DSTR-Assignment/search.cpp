2d1f02448ba93b74c115f99a1281a5c79e21977c
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2022-05-19 21:17:37 +0800

    feat(search): binary search

--- DSTR-Assignment/search.cpp
+++ DSTR-Assignment/search.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 
+#include "binary_tree.h"
 #include "display.h"
 #include "search.h"
 
@@ -8,20 +9,38 @@ using namespace std;
 
 TutorList searchTutor(TutorList &ll, int (*CompareFn)(Tutor *, Tutor *), Tutor *t)
 {
-	TutorNode *temp = ll.head;
-	TutorList tempList;
+	BinaryTree bt = BinaryTree(ll, (*CompareFn), 'a');
 
-	while (temp != nullptr)
+	TutorNode *treeNodePtr = bt.root;
+	TutorList tempList;
+	tempList.copy = true;
+
+	/**
+	 * Binary search
+	 */
+	while (treeNodePtr != nullptr)
 	{
-		int result = (*CompareFn)(temp->tutor, t);
+		int result = (*CompareFn)(t, treeNodePtr->tutor);
 		if (result == 0)
 		{
-			tempList.AddToLast(temp->tutor);
+			tempList.AddToLast(treeNodePtr->tutor);
+			/**
+			 * Since our binary tree is constructed in ascending order and
+			 * every node left sub-tree always contains nodes with value
+			 * smaller than or equal to the node, we traverse to the left
+			 * sub-tree
+			 */
+			treeNodePtr = treeNodePtr->prev;
 		}
-
-		temp = temp->next;
+		else if (result > 0)
+		{
+			treeNodePtr = treeNodePtr->next;
+		}
+		else
+		{
+			treeNodePtr = treeNodePtr->prev;
+		}
 	}
-	tempList.copy = true;
 
 	return tempList;
 }
